884327b837761139fea2a05a5e894ee994e05578
devin-bielejec <devin.bieleje@gmail.com>
2019-09-18 12:57:21 -0400

    validateUser middleware complete

--- server.js
+++ server.js
@@ -18,7 +18,18 @@ const logger = (req, res, next) => {
 const validateUserId = (req, res, next) => {
   const { id } = req.params;
   id ? req.user = { id } : res.status(400).json({message: "invalid user id"})
+  next();
 }
+
+const validateUser = (req, res, next) => {
+  req.body ? null : res.status(400).json({message: "missing user data"})
+
+  req.body.name ? null : res.status(400).json({message: "missing required name field"});
+
+  next();
+}
+
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
