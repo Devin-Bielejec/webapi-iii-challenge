f857629c98e0d9fe862909fb66461aa74778b152
devin-bielejec <devin.bieleje@gmail.com>
2019-09-18 12:55:07 -0400

    validateuserid middleware complete

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const express = require("express");
 
 const server = expres();
 
-const logger = (req, res, next) => {
-    console.log(
-        `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get("Origin")}`
-    )   
-}
+
 
 server.use(logger);
 
--- server.js
+++ server.js
@@ -2,14 +2,31 @@ const express = 'express';
 
 const server = express();
 
+server.use(logger);
+
+//custom middleware
+
+const logger = (req, res, next) => {
+  console.log(
+      `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get("Origin")}`
+  )   
+
+  next();
+}
+
+
+const validateUserId = (req, res, next) => {
+  const { id } = req.params;
+  id ? req.user = { id } : res.status(400).json({message: "invalid user id"})
+}
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
-//custom middleware
 
-function logger(req, res, next) {
 
-};
+
+
 
 module.exports = server;
