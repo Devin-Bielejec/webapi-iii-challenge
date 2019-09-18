3eaacc20a7de16a58b32da3427306681e4387aa4
devin-bielejec <devin.bieleje@gmail.com>
2019-09-18 15:59:04 -0400

    update user complete

--- users/userRouter.js
+++ users/userRouter.js
@@ -52,20 +52,24 @@ router.delete('/:id', validateUserId, (req, res) => {
 
     db.remove(id)
     .then(users => res.status(200).json(users))
-    .catch(err => res.status(500).json({message: "Server Error"}))
+    .catch(err => res.status(500).json({message: "Server Error while deleting"}))
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
 
+    db.update(id, {name})
+    .then(users => {
+        res.status(200).json(users);
+    })
+        .catch(err => res.status(500).json({message: "Server error while updating"}))
 });
-
-//custom middleware
 
 function validateUserId(req, res, next) {
     const { id } = req.params;
     db.getById(id)
     .then( (users) => {
-        console.log(users);
         users === undefined ? res.status(400).json({message: "user id not found"}) : req.user = { id };
     })
     .catch(err => res.status(500).json({message: "Server error retrieving id"}))
@@ -73,8 +77,6 @@ function validateUserId(req, res, next) {
 };
 
 function validateUser(req, res, next) {
-    console.log("here");
-    console.log(req.body);
     req.body ? null : res.status(400).json({message: "missing user data"})
     req.body.name ? null : res.status(400).json({message: "missing required name field"});
     next();
